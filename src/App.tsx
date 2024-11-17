cfc37fa872a61eda77f88e99dd27b6bcb5114efa
Fernando Vargas <fernan.assis@hotmail.com>
2024-11-16 22:05:13 -0300

    estilizations-and-rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
 
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+type Players = "O" | "X";
 
 function App() {
 
-  const getSquares = () => {
+  const [turn, setTurn] = useState<Players>("O");
+  const [winnwer,setWinner] = useState<Players | null>(null);
+  const [draw, setDraw] = useState<boolean | null>(null);
+  const [marks, setMarks] = useState<{[key:string]:Players}>({});
+
+const getSquares = () => {
       return new Array(9).fill(true);
+  }
+
+  const play = (index:number)=>{
+    setMarks(prev =>({ ...prev, [index]:turn}))
+    setTurn(prev => prev === "O" ? "X" : "O")
+  }
+
+  const getCellPlayer = (index: number) => {
+    if(!marks[index]){
+      return;
+    }
+    return marks[index];
   }
   
 
   return (
-    <div className='container'>
+    <div className="container">
 
       <h1>O ganhou</h1>
       <h1>Empate</h1>
@@ -18,10 +38,10 @@ function App() {
       <p>É a vez de X</p>
 
       <div className="board">
-      {getSquares().map((_,i) => (
-    <div className='cell'>X</div>))}
-
-        </div>    
+      {getSquares().map((_, i) => (
+        <div className={`cell ${getCellPlayer(i)}`} onClick={() => play(i)}>{marks[i]}</div>
+        ))}
+      </div>    
     </div>
   )
 }
